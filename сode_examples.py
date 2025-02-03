444a38e3caa3e3296b4c73884d1f0545279d5959
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-02-03 17:35:07 +0400

    Add files via upload

---
+++ сode_examples.py
@@ -0,0 +1,27 @@
+# 003
+# print('003')
+# print('What do you call a bear with no teeth? \nA gummy bear!')
+
+# 004
+# print('004')
+# one = float(input('Введите число 1: '))
+# two = float(input('Введите число 2: '))
+# print(f'The total is: {one + two}')
+
+# 005
+# print('005')
+# one = float(input('Введите число 1: '))
+# two = float(input('Введите число 2: '))
+# three = float(input('Введите число 3: '))
+# print(f'The answer is: {(one + two) * three}')
+
+# 006
+# one = float(input('Сколько кусков пиццы у Вас есть: '))
+# two = float(input('Сколько кусков пиццы Вы съели: '))
+# print(f'У Вас осталось: {one - two} куска')
+
+# 008
+# one = float(input('Введите общую сумму счета: '))
+# two = float(input('Введите количество участников: '))
+# print(f'Каждый участник должен заплатить: {one / two} рубля')
+
