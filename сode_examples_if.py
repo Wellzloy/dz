edc7842950958ec65a3603fecc41563c42ff0838
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-02-04 22:42:46 +0400

    Add files via upload

---
+++ сode_examples_if.py
@@ -0,0 +1,43 @@
+import time
+
+print(time.strftime('%H:%M:%S %d-%m-%Y %A %B ', time.localtime()))
+
+# 012
+# num1 = int(input('Введите первое число: '))
+# num2 = int(input('Введите второе число: '))
+# if num1 > num2:
+#     print(num2, num1)
+# else:
+#     print(num1, num2)
+# 013
+# num1 = int(input('Введите первое число меньше 20: '))
+# if num1 >= 20:
+#     print('Too high')
+# else:
+#     print('Thank you')
+# 014
+# num1 = int(input('Введите число от 10 до 20 включительно: '))
+# if 10 <= num1 <= 20:
+#     print('Thank you')
+# else:
+#     print('Incorrect answer')
+# 015
+# color = str(input('Введите любимый цвет: '))
+# color1 = color.lower()
+# if color1 == 'red':
+#     print(f'I like {color} too')
+# else:
+#     print(f"I don't like {color}, i prefer red")
+# 016
+# answer = input('Идет ли дождь? ')
+# answer = answer.lower()
+# if answer == 'yes':
+#     answer1 = input('Ветрено ли на улице? ')
+#     answer1 = answer1.lower()
+#     if answer1 == 'yes':
+#         print('Сейчас слишком ветрено для зонта')
+#     else:
+#         print('Возьмите зонтик')
+# else:
+#     print('Приятного Вам дня')
+
