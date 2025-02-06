551a430fc6305b9f8ca42f92caaab5f3caaf0d07
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2025-02-06 16:52:38 +0400

    Add files via upload

---
+++ сode_examples_str.py
@@ -0,0 +1,33 @@
+import time
+
+print(time.strftime('%H:%M:%S %d-%m-%Y %A %B ', time.localtime()))
+# 020
+# name = input('Введите Ваше имя: ')
+# print(f'Длинна имени {len(name)} символа')
+# 021
+# name = input('Введите Ваше имя: ')
+# surname = input('Введите Вашу фамилию: ')
+# name_surname = name + ' ' + surname
+# print(name_surname)
+# print(name, surname)
+# print(name, len(name))
+# 022
+# name = input('Введите Ваше имя: ')
+# surname = input('Введите Вашу фамилию: ')
+# name_surname = name + ' ' + surname
+# print(name_surname.title())
+# 023
+# words = input('Введите первую строку стихотворения: ')
+# print(len(words))
+# start = int(input(f'Введите начало отрезка от 0 до {len(words)}: '))
+# end = int(input(f'Введите окончание отрезка не менее {start} не более {len(words)}: '))
+# print(words[start:end])
+# 025
+# name = input('Введите Ваше имя: ')
+# if len(name) < 5:
+#     surname = input('Введите Вашу фамилию: ')
+#     print(name.upper() + surname.upper())
+# else:
+#     print(name.lower())
+
+
